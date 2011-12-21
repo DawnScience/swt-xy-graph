bda69df0649fcbd11262edb028063f1a830c0c71
ztyaner11 <ztyaner11@gmail.com>
2011-12-21 12:32:02 -0500

    ff

--- RCP_RAP/org.csstudio.swt.xygraph/src/org/csstudio/swt/xygraph/linearscale/LinearScaleTickLabels.java
+++ RCP_RAP/org.csstudio.swt.xygraph/src/org/csstudio/swt/xygraph/linearscale/LinearScaleTickLabels.java
@@ -504,8 +504,6 @@ public class LinearScaleTickLabels extends Figure {
         }
         	
         
-        // gridStepHint --> mantissa * 10 ** exponent
-        // e.g. 724.1 --> 7.241 * 10 ** 2
         double mantissa = gridStepHint;
         int exponent = 0;
         if (mantissa < 1) {
@@ -521,22 +519,17 @@ public class LinearScaleTickLabels extends Figure {
             }
         }
 
-        // calculate the grid step with hint.
         BigDecimal gridStep;
-        if (mantissa > 7.5) {
-            // gridStep = 10.0 * 10 ** exponent
-            gridStep = BigDecimal.TEN.multiply(pow(10, exponent));
-        } else if (mantissa > 3.5) {
-            // gridStep = 5.0 * 10 ** exponent
-            gridStep = new BigDecimal(new Double(5).toString()).multiply(pow(
+        if (mantissa > 7.5) {            
+            gridStep = BigDecimal.TEN.multiply(pow(10, exponent)); // 10.0 * 10 ** exponent 
+        } else if (mantissa > 3.5) {           
+            gridStep = new BigDecimal(new Double(5).toString()).multiply(pow(  // 5.0 * 10 ** exponent
                     10, exponent));
-        } else if (mantissa > 1.5) {
-            // gridStep = 2.0 * 10 ** exponent
-            gridStep = new BigDecimal(new Double(2).toString()).multiply(pow(
+        } else if (mantissa > 1.5) {            
+            gridStep = new BigDecimal(new Double(2).toString()).multiply(pow( // 2.0 * 10 ** exponent
                     10, exponent));
-        } else {
-            // gridStep = 1.0 * 10 ** exponent
-            gridStep = pow(10, exponent);
+        } else {            
+            gridStep = pow(10, exponent); // 1.0 * 10 ** exponent
         }
         if(minBigger)
         	gridStep = gridStep.negate();
