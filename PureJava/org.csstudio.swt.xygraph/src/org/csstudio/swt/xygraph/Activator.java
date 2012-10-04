d953dc7b274429ea388f1dcbb5e4b8ac7ca20aa0
matthew.gerring <matthew.gerring@gmail.com>
2012-10-04 14:23:31 +0100

    Removed incorrect dawn names in plugin.

--- PureJava/org.csstudio.swt.xygraph/src/org/csstudio/swt/xygraph/Activator.java
+++ PureJava/org.csstudio.swt.xygraph/src/org/csstudio/swt/xygraph/Activator.java
@@ -22,7 +22,7 @@ import org.osgi.framework.BundleContext;
 public class Activator extends AbstractUIPlugin {
 
 	/** The plug-in ID defined in MANIFEST.MF */
-	public static final String PLUGIN_ID = "org.dawb.org.csstudio.swt.xygraph"; //$NON-NLS-1$
+	public static final String PLUGIN_ID = "org.csstudio.swt.xygraph"; //$NON-NLS-1$
 
 	// The shared instance
 	private static Activator plugin;
