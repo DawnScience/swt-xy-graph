1d85fbb6fa31d7f0fce6d3cd33b3a4fab7fb047b
Peter Chang <Peter.Chang@diamond.ac.uk>
2013-01-25 16:25:59 +0000

    Improved tick factory by capturing many corner cases and added many more unit tests

--- PureJava/org.csstudio.swt.xygraph/src/org/csstudio/swt/xygraph/linearscale/TickFactory.java
+++ PureJava/org.csstudio.swt.xygraph/src/org/csstudio/swt/xygraph/linearscale/TickFactory.java
@@ -17,9 +17,8 @@
 package org.csstudio.swt.xygraph.linearscale;
 
 import java.math.BigDecimal;
-import java.text.DecimalFormat;
-import java.text.Format;
-import java.util.LinkedList;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Tick factory produces the different axis ticks. When specifying a format and
@@ -55,11 +54,16 @@ public class TickFactory {
 	private final static BigDecimal EPSILON = new BigDecimal("1.0E-20");
 	private static final int DIGITS_UPPER_LIMIT = 6; // limit for number of digits to display left of decimal point
 	private static final int DIGITS_LOWER_LIMIT = -6; // limit for number of zeros to display right of decimal point
+	private static final double ROUND_FRACTION = 2e-6; // fraction of denominator to round to
+	private static final BigDecimal BREL_ERROR = new BigDecimal("1e-15");
+	private static final double REL_ERROR = BREL_ERROR.doubleValue();
 	
 	private double graphMin;
 	private double graphMax;
-	private Object tickFormat; // Could be String or SimpleDecimalFormat
+	private String tickFormat;
 	private IScaleProvider scale;
+	private int intervals; // number of intervals
+	private boolean isReversed;
 	
 	/**
 	 * @param format
@@ -79,19 +83,17 @@ public class TickFactory {
 	private String getTickString(double value) {
 		
 		if (scale!=null) value = scale.getLabel(value);
-		if (Double.isNaN(value)) return "";
 		
 		String returnString = "";
+		if (Double.isNaN(value))
+			return returnString;
+
 		switch (formatOfTicks) {
 		case plainMode:
-			returnString = tickFormat instanceof Format
-			             ? ((Format)tickFormat).format(value)
-			             : String.format((String)tickFormat, value);
+			returnString = String.format(tickFormat, value);
 			break;
 		case useExponent:
-			returnString = tickFormat instanceof Format
-                         ? ((Format)tickFormat).format(value)
-                         : String.format((String)tickFormat, value);
+			returnString = String.format(tickFormat, value);
 			break;
 		case roundAndChopMode:
 			returnString = String.format("%d", Math.round(value));
@@ -130,59 +132,19 @@ public class TickFactory {
 		return returnString;
 	}
 
-	/**
-	 * @param x
-	 * @return floor of log 10
-	 */
-	private int log10(BigDecimal x) {
-		int c = x.compareTo(BigDecimal.ONE); 
-		int e = 0;
-		while (c < 0) {
-			e--;
-			x = x.scaleByPowerOfTen(1);
-			c = x.compareTo(BigDecimal.ONE);
+	private void createFormatString(final int precision, final boolean b) {
+		switch (formatOfTicks) {
+		case plainMode:
+			tickFormat = b ? String.format("%%.%de", precision) : String.format("%%.%df", precision);
+			break;
+		case useExponent:
+			tickFormat = String.format("%%.%de", precision);
+			break;
+		default:
+			tickFormat = null;
+			break;
 		}
-	
-		c = x.compareTo(BigDecimal.TEN);
-		while (c >= 0) {
-			e++;
-			x = x.scaleByPowerOfTen(-1);
-			c = x.compareTo(BigDecimal.TEN);
-		}
-	
-		return e;
 	}
-
-	private BigDecimal nicenum(BigDecimal x, boolean round) {
-		int expv; /* exponent of x */
-		double f; /* fractional part of x */
-		double nf; /* nice, rounded number */
-		BigDecimal bf;
-
-		expv = log10(x);
-		bf = x.scaleByPowerOfTen(-expv);
-		f = bf.doubleValue();
-		// f = x/Math.pow(10., expv); /* between 1 and 10 */
-		if (round)
-			if (f < 1.5)
-				nf = 1.;
-			else if (f < 3.)
-				nf = 2.;
-			else if (f < 7.)
-				nf = 5.;
-			else
-				nf = 10.;
-		else if (f <= 1.)
-			nf = 1.;
-		else if (f <= 2.)
-			nf = 2.;
-		else if (f <= 5.)
-			nf = 5.;
-		else
-			nf = 10.;
-		return BigDecimal.valueOf(nf).scaleByPowerOfTen(expv);
-	}
-
 
 	/**
 	 * Round numerator down to multiples of denominators 
@@ -201,6 +163,14 @@ public class TickFactory {
 		n = n.abs();
 		d = d.abs();
 		final BigDecimal[] x = n.divideAndRemainder(d);
+		double rx = x[1].doubleValue();
+		if (rx > (1-ROUND_FRACTION)*d.doubleValue()) {
+			// trim up if close to denominator
+			x[1] = BigDecimal.ZERO;
+			x[0] = x[0].add(BigDecimal.ONE);
+		} else if (rx < ROUND_FRACTION*d.doubleValue()) {
+			x[1] = BigDecimal.ZERO;
+		}
 		final int xs = x[1].signum();
 		if (xs == 0) {
 			return ns != ds ? -x[0].multiply(d).doubleValue() : x[0].multiply(d).doubleValue();
@@ -231,6 +201,16 @@ public class TickFactory {
 		n = n.abs();
 		d = d.abs();
 		final BigDecimal[] x = n.divideAndRemainder(d);
+		double rx = x[1].doubleValue();
+		if (rx != 0) {
+			if (rx < ROUND_FRACTION*d.doubleValue()) {
+				// trim down if close to zero
+				x[1] = BigDecimal.ZERO;
+			} else if (rx > (1-ROUND_FRACTION)*d.doubleValue()) {
+				x[1] = BigDecimal.ZERO;
+				x[0] = x[0].add(BigDecimal.ONE);
+			}
+		}
 		final int xs = x[1].signum();
 		if (xs == 0) {
 			return ns != ds ? -x[0].multiply(d).doubleValue() : x[0].multiply(d).doubleValue();
@@ -244,83 +224,132 @@ public class TickFactory {
 		return x[0].add(BigDecimal.ONE).multiply(d).doubleValue();
 	}
 
-	
-	private void createFormatString(final int precision, final boolean b) {
-		switch (formatOfTicks) {
-		case plainMode:
-			if (precision==0) { // 
-				tickFormat = new DecimalFormat("#####0.###");
-				break;
-			}
-			tickFormat = b ? String.format("%%.%de", precision) : String.format("%%.%df", precision);
-			break;
-		case useExponent:
-			tickFormat = String.format("%%.%de", precision);
-			break;
-		default:
-			tickFormat = null;
-			break;
+	/**
+	 * @param x
+	 * @return floor of log 10
+	 */
+	private static int log10(BigDecimal x) {
+		int c = x.compareTo(BigDecimal.ONE); 
+		int e = 0;
+		while (c < 0) {
+			e--;
+			x = x.scaleByPowerOfTen(1);
+			c = x.compareTo(BigDecimal.ONE);
 		}
+	
+		c = x.compareTo(BigDecimal.TEN);
+		while (c >= 0) {
+			e++;
+			x = x.scaleByPowerOfTen(-1);
+			c = x.compareTo(BigDecimal.TEN);
+		}
+	
+		return e;
 	}
 
-	/*
-	 *  TODO include zero tick if range straddles origin
-	 *  Split into lower and upper ranges and use larger range?
-	 *  How do we reconcile with maxTicks?
-	 *  Or check if zero is included first?
+	/**
+	 * @param x
+	 * @param round if true, then round else take ceiling
+	 * @return a nice number
 	 */
+	protected static BigDecimal nicenum(BigDecimal x, boolean round) {
+			int expv; /* exponent of x */
+			double f; /* fractional part of x */
+			double nf; /* nice, rounded number */
+			BigDecimal bf;
+	
+			expv = log10(x);
+			bf = x.scaleByPowerOfTen(-expv);
+			f = bf.doubleValue(); /* between 1 and 10 */
+			if (round) {
+				if (f < 1.5)
+					nf = 1;
+				else if (f < 2.25)
+					nf = 2;
+				else if (f < 3.25)
+					nf = 2.5;
+				else if (f < 7.5)
+					nf = 5;
+				else
+					nf = 10;
+			}
+			else if (f <= 1.)
+				nf = 1;
+			else if (f <= 2.)
+				nf = 2;
+			else if (f <= 5.)
+				nf = 5;
+			else
+				nf = 10;
+		return BigDecimal.valueOf(BigDecimal.valueOf(nf).scaleByPowerOfTen(expv).doubleValue());
+	}
 
 	private double determineNumTicks(int size, double min, double max, int maxTicks, boolean allowMinMaxOver) {
 		BigDecimal bMin = BigDecimal.valueOf(min);
 		BigDecimal bMax = BigDecimal.valueOf(max);
 		BigDecimal bRange = bMax.subtract(bMin);
-		final boolean isReverse;
 		if (bRange.signum() < 0) {
 			BigDecimal bt = bMin;
 			bMin = bMax;
 			bMax = bt;
 			bRange = bRange.negate();
-			isReverse = true;
+			isReversed = true;
 		} else {
-			isReverse = false;
+			isReversed = false;
 		}
-	
+
+		BigDecimal magnitude = BigDecimal.valueOf(Math.max(Math.abs(min), Math.abs(max)));
 		// tick points too dense to do anything
-		if (bRange.compareTo(EPSILON) < 0) {
+		if (bRange.compareTo(EPSILON.multiply(magnitude)) < 0) {
 			return 0;
 		}
 	
 		bRange = nicenum(bRange, false);
 		BigDecimal bUnit;
-		long n;
-		do { // ensure number of ticks is less or equal to number requested
-			bUnit = nicenum(BigDecimal.valueOf(bRange.doubleValue() / (maxTicks - 1)), true);
-			n = bRange.divideToIntegralValue(bUnit).longValue();
-		} while (n > maxTicks-- && maxTicks > 1);
-
-		double tickUnit = isReverse ? -bUnit.doubleValue() : bUnit.doubleValue();
-		if (allowMinMaxOver) {
-			graphMin = roundDown(bMin, bUnit);
-			graphMax = roundUp(bMax, bUnit);
-		} else {
-			graphMin = min;
-			graphMax = max;
+		int nTicks = maxTicks - 1;
+		if (Math.signum(min)*Math.signum(max) < 0) {
+			// straddle case
+			nTicks++;
 		}
-		if (isReverse) {
+		do {
+			long n;
+			do { // ensure number of ticks is less or equal to number requested
+				bUnit = nicenum(BigDecimal.valueOf(bRange.doubleValue() / nTicks), true);
+				n = bRange.divideToIntegralValue(bUnit).longValue();
+			} while (n > maxTicks && --nTicks > 0);
+
+			if (allowMinMaxOver) {
+				graphMin = roundDown(bMin, bUnit);
+				if (graphMin == 0) // ensure positive zero
+					graphMin = 0;
+				graphMax = roundUp(bMax, bUnit);
+				if (graphMax == 0)
+					graphMax = 0;
+			} else {
+				graphMin = min;
+				graphMax = max;
+			}
+			if (bUnit.compareTo(BREL_ERROR.multiply(magnitude)) <= 0) {
+				intervals = -1; // signal that we hit the limit of precision
+			} else {
+				intervals = (int) Math.round((graphMax - graphMin) / bUnit.doubleValue());
+			}
+		} while (intervals > maxTicks && --nTicks > 0);
+		if (isReversed) {
 			double t = graphMin;
 			graphMin = graphMax;
 			graphMax = t;
 		}
+		double tickUnit = isReversed ? -bUnit.doubleValue() : bUnit.doubleValue();
 
 		/**
-		 * We get the labelled max and minf for determining the 
+		 * We get the labelled max and min for determining the 
 		 * precision which the ticks should be shown at.
 		 */
-		// Following block seems to replicate part of what DecimalFormat does...
-		int d = (int) Math.floor(Math.log10(Math.abs(tickUnit))); // number of digits required
-		if (d <= DIGITS_LOWER_LIMIT || d >= DIGITS_UPPER_LIMIT) {
-			int p = (int) Math.max(Math.floor(Math.log10(Math.abs(min))),
-					               Math.floor(Math.log10(Math.abs(max))));
+		int d = bUnit.scale() == bUnit.precision() ?  -bUnit.scale() : bUnit.precision() - bUnit.scale() - 1;
+		int p = (int) Math.max(Math.floor(Math.log10(Math.abs(min))), Math.floor(Math.log10(Math.abs(max))));
+//		System.err.println("P: " + bUnit.precision() + ", S: " + bUnit.scale() + " => " + d + ", " + p);
+		if (p <= DIGITS_LOWER_LIMIT || p >= DIGITS_UPPER_LIMIT) {
 			createFormatString(Math.max(p - d, 0), true);
 		} else {
 			createFormatString(Math.max(-d, 0), false);
@@ -328,84 +357,104 @@ public class TickFactory {
 		return tickUnit;
 	}
 
+	private boolean inRange(double x, double min, double max) {
+		if (isReversed) {
+			return x >= max && x <= min;
+		}
+		return x >= min && x <= max;
+	}
+
 	/**
+	 * Generate a list of ticks that span range given by min and max. The maximum number of
+	 * ticks is exceed by one in the case where the range straddles zero.
 	 * @param displaySize 
 	 * @param min
 	 * @param max
-	 * @param maxTicks
+	 * @param maxTicks 
 	 * @param allowMinMaxOver allow min/maximum overwrite
 	 * @param tight if true then remove ticks outside range 
 	 * @return a list of the ticks for the axis
 	 */
-	public LinkedList<Tick> generateTicks(int displaySize, double min, double max, int maxTicks,
+	public List<Tick> generateTicks(int displaySize, double min, double max, int maxTicks,
 										  boolean allowMinMaxOver, final boolean tight)
 	{
-		LinkedList<Tick> ticks = new LinkedList<Tick>();
+		List<Tick> ticks = new ArrayList<Tick>();
 		double tickUnit = determineNumTicks(displaySize, min, max, maxTicks, allowMinMaxOver);
-		double p = graphMin;
-		if (tickUnit > 0) {
-			final double pmax = graphMax + 0.5 * tickUnit;
-			int i = 0;
-			while (p < pmax) {
-				if (!tight || (p >= min && p <= max))
-					if (allowMinMaxOver || p <= max) {
-						Tick newTick = new Tick();
-						newTick.setValue(p);
-						newTick.setText(getTickString(p));
-						ticks.add(newTick);
-					}
-				double newTickValue = graphMin + (++i)*tickUnit;
-				if (p == newTickValue)
-					break;
-				else if (p < graphMax && newTickValue > graphMax)
-					p = graphMax; // if slightly exceed maximum (zero corner case)
-				else
-					p = newTickValue;
+		if (tickUnit == 0)
+			return ticks;
+
+		for (int i = 0; i <= intervals; i++) {
+			double p = graphMin + i * tickUnit;
+			if (Math.abs(p/tickUnit) < REL_ERROR)
+				p = 0; // ensure positive zero
+			boolean r = inRange(p, min, max);
+			if (!tight || r) {
+				Tick newTick = new Tick();
+				newTick.setValue(p);
+				newTick.setText(getTickString(p));
+				ticks.add(newTick);
 			}
-			final int imax = ticks.size();
-			if (imax == 1) {
-				ticks.get(0).setPosition(0.5);
-			} else if (imax > 1) {
-				double lo = tight ? min : ticks.get(0).getValue();
-				double hi = tight ? max : ticks.get(imax - 1).getValue();
-				double range = hi - lo;
+		}
+
+		int imax = ticks.size();
+		if (imax > 1) {
+			if (!tight && allowMinMaxOver) {
+				Tick t = ticks.get(imax - 1);
+				if (tickUnit > 0 && t.getValue() < max) { // last is >= max
+					t.setValue(graphMax);
+					t.setText(getTickString(graphMax));
+				}
+			}
+
+			double lo = tight ? min : ticks.get(0).getValue();
+			double hi = tight ? max : ticks.get(imax - 1).getValue();
+			double range = hi - lo;
+
+			if (tickUnit > 0) {
 				for (Tick t : ticks) {
 					t.setPosition((t.getValue() - lo) / range);
 				}
-			}
-		} else if (tickUnit < 0) {
-			final double pmin = graphMax + 0.5 * tickUnit;
-			while (p > pmin) {
-				if (!tight || (p >= max && p <= min))
-					if (allowMinMaxOver || p <= max) {
-						Tick newTick = new Tick();
-						newTick.setValue(p);
-						newTick.setText(getTickString(p));
-						ticks.add(newTick);
-					}
-				double newTickValue = p + tickUnit;
-				if (p == newTickValue)
-					break;
-				else if (p < graphMax && newTickValue > graphMax)
-					p = graphMax; // if slightly exceed maximum (zero corner case)
-				else
-					p = newTickValue;
-			}
-			final int imax = ticks.size();
-			if (imax == 1) {
-				ticks.get(0).setPosition(0.5);
-			} else if (imax > 1) {
-				double lo = tight ? max : ticks.get(0).getValue();
-				double hi = tight ? min : ticks.get(imax - 1).getValue();
-				double range = hi - lo;
+			} else {
 				for (Tick t : ticks) {
 					t.setPosition(1 - (t.getValue() - lo) / range);
+				}
+			}
+		} else if (maxTicks > 1) {
+			if (imax == 0) {
+				imax++;
+				Tick newTick = new Tick();
+				newTick.setValue(graphMin);
+				newTick.setText(getTickString(graphMin));
+				if (tickUnit < 0) {
+					newTick.setPosition(1);
+				} else {
+					newTick.setPosition(0);
+				}
+				ticks.add(newTick);
+			}
+			if (imax == 1) {
+				Tick t = ticks.get(0);
+				Tick newTick = new Tick();
+				if (t.getText().equals(getTickString(graphMax))) {
+					newTick.setValue(graphMin);
+					newTick.setText(getTickString(graphMin));
+					ticks.add(0, newTick);
+				} else {
+					newTick.setValue(graphMax);
+					newTick.setText(getTickString(graphMax));
+					ticks.add(newTick);
+				}
+				if (tickUnit < 0) {
+					ticks.get(0).setPosition(1);
+					ticks.get(1).setPosition(0);
+				} else {
+					ticks.get(0).setPosition(1);
+					ticks.get(1).setPosition(0);
 				}
 			}
 		}
 		return ticks;
 	}
-
 
 	private double determineNumLogTicks(int size, double min, double max, int maxTicks, boolean allowMinMaxOver) {
 		final boolean isReverse = min > max;
@@ -454,10 +503,10 @@ public class TickFactory {
 	 * @param tight if true then remove ticks outside range 
 	 * @return a list of the ticks for the axis
 	 */
-	public LinkedList<Tick> generateLogTicks(int displaySize, double min, double max, int maxTicks,
+	public List<Tick> generateLogTicks(int displaySize, double min, double max, int maxTicks,
 										  boolean allowMinMaxOver, final boolean tight)
 	{
-		LinkedList<Tick> ticks = new LinkedList<Tick>();
+		List<Tick> ticks = new ArrayList<Tick>();
 		double tickUnit = determineNumLogTicks(displaySize, min, max, maxTicks, allowMinMaxOver);
 		double p = graphMin;
 		if (tickUnit > 1) {
