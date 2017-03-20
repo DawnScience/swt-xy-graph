eclipse/nebula

Bug 452686 - CircularBufferDataProvider add data array and clearTrace() doesn't work properly
Fixed.

Change-Id: I125f0f02b20a31c50e14948b5f2b39a20b1b087b

widgets/visualization/org.eclipse.nebula.visualization.xygraph/src/org/eclipse/nebula/visualization/xygraph/dataprovider/CircularBufferDataProvider.java
@@ -334,15 +334,24 @@ public class CircularBufferDataProvider extends AbstractDataProvider{
 			}
 		}
 		
-			currentXDataChanged = false;
-			currentYDataChanged = false;
+			currentXDataArrayChanged = false;
+			currentYDataArrayChanged = false;
 //			currentYDataTimestampChanged = false;
 			fireDataChange();		
 	}
 	
 
+	/**
+	 * Clear all data on in the data provider.
+	 */
 	public synchronized void clearTrace(){
 		traceData.clear();
+		currentXDataArray=new double[]{};
+		currentYDataArray = new double[]{};
+		currentXDataChanged = false;
+		currentYDataChanged = false;
+		currentXDataArrayChanged = false;
+		currentYDataArrayChanged = false;
 		fireDataChange();
 	}
 	

DawnScience/swt-xy-graph

PureJava/org.eclipse.nebula.visualization.xygraph/src/org/eclipse/nebula/visualization/xygraph/dataprovider/CircularBufferDataProvider.java
@@ -336,14 +336,23 @@ public class CircularBufferDataProvider extends AbstractDataProvider {
 			}
 		}
 
-		currentXDataChanged = false;
-		currentYDataChanged = false;
+		currentXDataArrayChanged = false;
+		currentYDataArrayChanged = false;
 		currentYDataTimestampChanged = false;
 		fireDataChange();
 	}
 
+	/**
+	 * Clear all data on in the data provider.
+	 */
 	public synchronized void clearTrace() {
 		traceData.clear();
+		currentXDataArray = new double[] {};
+		currentYDataArray = new double[] {};
+		currentXDataChanged = false;
+		currentYDataChanged = false;
+		currentXDataArrayChanged = false;
+		currentYDataArrayChanged = false;
 		fireDataChange();
 	}
 