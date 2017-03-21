1a565ea71c6cecce7edb5111122385ca86635789
Jonah Graham <jonah@kichwacoders.com>
2017-03-21 17:06:56 +0000

    Update GraphicsUtils with new drawVerticalText from upstream
    Signed-off-by: Jonah Graham <jonah@kichwacoders.com>

--- PureJava/org.eclipse.nebula.visualization.xygraph/src/org/eclipse/nebula/visualization/xygraph/util/GraphicsUtil.java
+++ PureJava/org.eclipse.nebula.visualization.xygraph/src/org/eclipse/nebula/visualization/xygraph/util/GraphicsUtil.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010 Oak Ridge National Laboratory.
+ * Copyright (c) 2010, 2017 Oak Ridge National Laboratory and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -9,26 +9,25 @@ package org.eclipse.nebula.visualization.xygraph.util;
 
 import org.eclipse.draw2d.FigureUtilities;
 import org.eclipse.draw2d.Graphics;
-import org.eclipse.draw2d.geometry.Dimension;
 import org.eclipse.draw2d.geometry.Point;
-import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Cursor;
 import org.eclipse.swt.graphics.Device;
 import org.eclipse.swt.graphics.Drawable;
 import org.eclipse.swt.graphics.GC;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.graphics.ImageData;
-import org.eclipse.swt.graphics.RGB;
 import org.eclipse.swt.graphics.Transform;
-import org.eclipse.swt.widgets.Display;
 
 /**
  * Utility function for graphics operations.
  * 
  * @author Xihui Chen
- *
+ * 
  */
 public final class GraphicsUtil {
+
+	private static boolean isRAP = SWT.getPlatform().startsWith("rap"); //$NON-NLS-1$ ;
 
 	/**
 	 * Draw vertical text.
@@ -44,54 +43,36 @@ public final class GraphicsUtil {
 	 */
 	public static final void drawVerticalText(Graphics graphics, String text, int x, int y, boolean upToDown) {
 		try {
-			graphics.pushState();
-			graphics.translate(x, y);
-			if (upToDown) {
-				graphics.rotate(90);
-				graphics.drawText(text, 0, -FigureUtilities.getTextExtents(text, graphics.getFont()).height);
-			} else {
-				graphics.rotate(270);
-				graphics.drawText(text, -FigureUtilities.getTextWidth(text, graphics.getFont()), 0);
-			}
-			graphics.popState();
-		} catch (Exception e) {// If rotate is not supported by the graphics.
-			graphics.popState();
-			final Dimension titleSize = FigureUtilities.getTextExtents(text, graphics.getFont());
-
-			final int w = titleSize.height;
-			final int h = titleSize.width + 1;
-			Image image = new Image(Display.getCurrent(), w, h);
+			if (SWT.getPlatform().startsWith("rap")) //$NON-NLS-1$
+				throw new Exception();
 			try {
-				final GC gc = createGC(image);
-				final Color titleColor = graphics.getForegroundColor();
-				RGB transparentRGB = new RGB(240, 240, 240);
-
-				gc.setBackground(XYGraphMediaFactory.getInstance().getColor(transparentRGB));
-				gc.fillRectangle(image.getBounds());
-				gc.setForeground(titleColor);
-				gc.setFont(graphics.getFont());
-				final Transform tr = new Transform(Display.getCurrent());
-				if (!upToDown) {
-					tr.translate(0, h);
-					tr.rotate(-90);
-					setTransform(gc, tr);
+				graphics.pushState();
+				graphics.translate(x, y);
+				if (upToDown) {
+					graphics.rotate(90);
+					graphics.drawText(text, 0, -FigureUtilities.getTextExtents(text, graphics.getFont()).height);
 				} else {
-					tr.translate(w, 0);
-					tr.rotate(90);
-					setTransform(gc, tr);
+					graphics.rotate(270);
+					graphics.drawText(text, -FigureUtilities.getTextWidth(text, graphics.getFont()), 0);
 				}
-				gc.drawText(text, 0, 0);
-				tr.dispose();
-				gc.dispose();
-				final ImageData imageData = image.getImageData();
-				image.dispose();
-				imageData.transparentPixel = imageData.palette.getPixel(transparentRGB);
-				image = new Image(Display.getCurrent(), imageData);
+			} finally {
+				graphics.popState();
+			}
+		} catch (Exception e) {// If rotate is not supported by the graphics.
+			// final Dimension titleSize = FigureUtilities.getTextExtents(text,
+			// graphics.getFont());
 
+			// final int w = titleSize.height;
+			// final int h = titleSize.width + 1;
+			Image image = null;
+			try {
+				image = SingleSourceHelper2.createVerticalTextImage(text, graphics.getFont(),
+						graphics.getForegroundColor().getRGB(), upToDown);
 				graphics.drawImage(image, x, y);
 
 			} finally {
-				image.dispose();
+				if (image != null)
+					image.dispose();
 			}
 		}
 	}
@@ -108,6 +89,10 @@ public final class GraphicsUtil {
 	 */
 	public static final void drawVerticalText(Graphics graphics, String text, Point location, boolean upToDown) {
 		drawVerticalText(graphics, text, location.x, location.y, upToDown);
+	}
+
+	public static final boolean isRAP() {
+		return isRAP;
 	}
 
 	/**
