68c07c15feab2452118cdb9e879194746baa06bf
Baha El Kassaby <baha.elkassaby@gmail.com>
2017-05-20 18:33:00 +0200

    add missing javadoc

--- PureJava/org.eclipse.nebula.visualization.xygraph/src/org/eclipse/nebula/visualization/xygraph/figures/IAnnotationLabelProvider.java
+++ PureJava/org.eclipse.nebula.visualization.xygraph/src/org/eclipse/nebula/visualization/xygraph/figures/IAnnotationLabelProvider.java
@@ -1,5 +1,21 @@
+/*******************************************************************************
+ * Copyright (c) 2017 Diamond Light Source Ltd
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Matthew Gerring - initial commit and implementation
+ ******************************************************************************/
 package org.eclipse.nebula.visualization.xygraph.figures;
 
+/**
+ * Interface used to get the information text of a label
+ *
+ * @author Matthew Gerring
+ *
+ */
 public interface IAnnotationLabelProvider {
 
 	/**
