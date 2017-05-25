9448c689eef312795cb0839c9601222d28bf5bd4
Baha El Kassaby <baha.elkassaby@gmail.com>
2017-05-25 13:13:56 +0200

    Fix javadoc

--- PureJava/org.eclipse.nebula.visualization.xygraph/src/org/eclipse/nebula/visualization/xygraph/linearscale/Tick.java
+++ PureJava/org.eclipse.nebula.visualization.xygraph/src/org/eclipse/nebula/visualization/xygraph/linearscale/Tick.java
@@ -1,5 +1,16 @@
+/*******************************************************************************
+ * Copyright (c) 2012, 2017 Diamond Light Source Ltd.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ ******************************************************************************/
 package org.eclipse.nebula.visualization.xygraph.linearscale;
 
+/**
+ * Tick object used in alternative tick mark generator {@link LinearScaleTicks2}
+ *
+ */
 public class Tick {
 
 	private String text;
